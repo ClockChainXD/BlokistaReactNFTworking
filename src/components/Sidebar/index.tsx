0b6013af54131de1d4b56e5ae0b1181a303ff1b4
ClockChainXD <85744377+ClockChainXD@users.noreply.github.com>
2021-08-11 15:41:20 +0300

    yes

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,8 +1,4 @@
-<<<<<<< Updated upstream
-import React, {Component} from 'react'
-=======
 import React, {Component, useState} from 'react'
->>>>>>> Stashed changes
 import {
     CDBSidebar,
     CDBSidebarContent,
@@ -11,17 +7,12 @@ import {
     CDBSidebarMenu,
     CDBSidebarMenuItem,
 } from 'cdbreact';
-<<<<<<< Updated upstream
-
-import { NavLink } from 'react-router-dom';
-=======
 import "./style.css"
 import { NavLink } from 'react-router-dom';
 import { Collapse, Button, Card, CardBody} from 'reactstrap';
 import { categoryOptions, userOptions } from '../../constants/filter';
 import SelectField from '../Forms/SelectField';
 import { makeStyles } from '@material-ui/core';
->>>>>>> Stashed changes
 
 let category = [
     {
@@ -73,144 +64,6 @@ let collections = [
         quantity : 125,
     },
 ]
-<<<<<<< Updated upstream
-/*  prefix={<i className='fa fa-bars fa-large'></i>} */
-
-class Sidebar extends Component{ 
-    render(){
-        return(
-            <div
-                style={{height: '50vh', overflow: 'scroll initial'}}
-            >
-                <CDBSidebar style={{backgroundColor: 'white', color:'black'}}>
-                    <CDBSidebarContent>
-                        <div className="container">
-                            <CDBSidebarMenu>
-                                <article className="card-group-item">
-                                    <header className="card-header">
-                                        <CDBSidebarMenuItem icon="plus-square">Status</CDBSidebarMenuItem>
-                                    </header>
-                                    <div className="filter-content" >
-                                        <div className="card-body">
-                                        <form>
-                                            <label className="form-check">
-                                            <input className="form-check-input" type="checkbox" value="" />
-                                            <span className="form-check-label">
-                                                Buy Now
-                                            </span>
-                                            </label> 
-                                            <label className="form-check">
-                                            <input className="form-check-input" type="checkbox" value="" />
-                                            <span className="form-check-label">
-                                                New(24 hours)
-                                            </span>
-                                            </label> 
-                                            <label className="form-check">
-                                            <input className="form-check-input" type="checkbox" value="" />
-                                            <span className="form-check-label">
-                                                Has Offers
-                                            </span>
-                                            </label>
-                                            <label className="form-check">
-                                            <input className="form-check-input" type="checkbox" value="" />
-                                            <span className="form-check-label">
-                                            On Auction
-                                            </span>
-                                        </label> 
-                                    </form>
-                                    </div> 
-                                    </div>
-                                </article> 
-                                <article className="card-group-item">
-                                    <header className="card-header">
-                                        <CDBSidebarMenuItem icon="tags">Price</CDBSidebarMenuItem>
-
-                                    </header>
-                                    <div className="filter-content">
-                                        <div className="card-body">
-                                            <div className="form-row">
-                                                <div className="form-group col-md-6">
-                                                    <label>Min</label>
-                                                    <input type="number" className="form-control" id="inputEmail4" placeholder="$0" />
-                                                </div>
-                                                <div className="form-group col-md-6 text-right">
-                                                    <label>Max</label>
-                                                    <input type="number" className="form-control" placeholder="$1,0000" />
-                                                </div>
-                                            </div>
-                                        </div> 
-                                    </div>
-                                </article> 
-                                <article className="card-group-item">
-                                    <CDBSidebarMenuItem icon="columns">Categories</CDBSidebarMenuItem>
-                                    <div className="filter-content">
-                                        <div className="list-group">
-                                            {category.map(item => {
-                                                console.log(item);
-                                                return(
-                                                    <NavLink to="" color="red" className="list-group-item" data-id={item.id}>
-                                                        {item.title}
-                                                        <span className="float-right badge badge-light round">{item.quantity}</span>
-                                                    </NavLink>
-                                                );
-                                            })}
-                                        </div>
-                                    </div>
-                                </article>
-                                <article className="card-group-item">
-                                    <header className="card-header">
-                                        <CDBSidebarMenuItem icon="users">Collections</CDBSidebarMenuItem>
-                                    </header>
-                                    <div className="list-group overflow-auto">
-                                                {
-                                                    collections.map(item => { console.log(item.title.length);
-
-                                                        item.title = item.title.toLowerCase()
-                                                        if(item.title.length > 10){
-                                                            item.title = item.title.slice(0, 10) + "...";
-                                                            console.log(item.title);
-                                                        }
-
-                                                        return(
-                                                            <NavLink to={"/"} className="list-group-item" data-id={"collection" + item.id}>
-                                                                <div className="custom-control custom-checkbox">
-                                                                    <img src={item.picture} style={{float: 'left', width: '30px', borderRadius: '50%', marginRight: '10px'}} />  
-                                                                    
-                                                                    <p className="text-left">
-                                                                        {item.title}
-                                                                        <span className="float-right badge badge-light round">
-                                                                            {item.quantity}
-                                                                        </span>
-                                                                    </p>
-                                                                </div>
-                                                            </NavLink>
-                                                            
-                                                        )
-                                                    })
-                                                }
-                                            </div>
-                                </article>                         
-                            </CDBSidebarMenu>
-                        </div>
-                    </CDBSidebarContent>
-        
-                    <CDBSidebarFooter>
-                        <div
-                            className="sidebar-btn-wrapper text-center"
-                            style={{padding:'20px 5px'}}
-                        >
-                            <NavLink
-                             to='/'>
-                                Blokista
-                            </NavLink>
-                        </div>
-                    </CDBSidebarFooter>
-                </CDBSidebar>
-        
-            </div>
-        )
-    }
-=======
 
 const useStyles = makeStyles(theme => ({
     main: {
@@ -311,7 +164,6 @@ function Sidebar(radioChangeHandler, selectChangeHandler ){
                 </CDBSidebar>
             </div>
         )
->>>>>>> Stashed changes
 }
 
 export default Sidebar;
